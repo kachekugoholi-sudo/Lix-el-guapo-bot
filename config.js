8b4e0f7f349666eff92b6fa245a63c3c0d373e3e
Félix Ofx <xlfelixgamer@gmail.com>
2026-07-30 00:08:50 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   name: 'Toji-Bot',
-  photo: 'https://raw.githubusercontent.com/kachekugoholi-sudo/imagenes-bot/f6ec78f43be4cba4ae2b8c0961862d2cfead00dc/360d8030fdca2805ab4b681db4>
+  photo: 'https://files.evogb.win/jwmu5N.jpg',
   owner: 'Naru-lix',
   version: '1.0.0',
   description: 'Bot de WhatsApp con estilo'
