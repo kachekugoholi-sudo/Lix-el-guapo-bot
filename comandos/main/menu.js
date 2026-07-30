cd9670b3f79153ad13ff7f2a76f74edb1d96a083
Félix Ofx <xlfelixgamer@gmail.com>
2026-07-29 23:58:57 -0400

    Actualizar menu.js

--- comandos/main/menu.js
+++ comandos/main/menu.js
@@ -13,18 +13,35 @@ export default {
 
       const menuTexto = `¡Hola @${userShortId}! soy Naru *(main)*
 
-╭────  🌻 𝐌̅𝐄꯭𝐍𝐔  🌼 ────╮
+╭────  🪾𝐌̅𝐄꯭𝐍𝐔🍂 ────╮
 
-- 𝐶𝑜𝑚𝑎𝑛𝑑𝑜𝑠 𝑑𝑖𝑠𝑝𝑜𝑛𝑖𝑏𝑙𝑒𝑠
+꒰🌕ᩚ꒱  ⣿   ☆  .𝗉𝗂𝗇𝗀
+> ✨ Latencia del bot.
+꒰🌕ᩚ꒱  ⣿   ☆  .𝗆𝖾𝗇𝗎
+> ✨ Muestra este menú.
+꒰🌕ᩚ꒱  ⣿   ☆  .𝗂𝗇𝖿𝗈
+> ✨ información del 𝖻𝗈𝗍
 
-꒰🌕ᩚ꒱  ⣿   ☆  ${prefijo}𝗉𝗂𝗇𝗀  - Latencia del bot
-꒰🌕ᩚ꒱  ⣿   ☆  ${prefijo}𝗆𝖾𝗇𝗎  -  Muestra este menú
-꒰🌕ᩚ꒱  ⣿   ☆  ${prefijo}𝗂𝗇𝖿𝗈  -  información del 𝖻𝗈𝗍
-꒰🌕ᩚ꒱  ⣿   ☆ ${prefijo}𝖺𝖽𝗆𝗂𝗇  -  Comandos de administración.
 
-𝑝𝑟𝑜𝑛𝑡𝑜 𝑠𝑒 𝑖𝑚𝑝𝑙𝑒𝑚𝑒𝑛𝑡𝑎𝑟𝑎𝑛 𝑚𝑎́𝑠 𝑐𝑜𝑠𝑎𝑠
-
-╰──── 🌼 𝐌̅𝐄꯭𝐍𝐔 🌻─────╯`;
+╭────  🍂𝐀𝐃𝐌𝐈𝐍𝐒🪾 ────╮
+꒰🌕ᩚ꒱  ⣿   ☆  .𝖺𝖽𝗆𝗂𝗇𝗌
+> ✨ Menciona a los administradores
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗍𝖺𝗀𝖺𝗅𝗅
+> ✨ 𝖬𝖾𝗇𝖼𝗂𝗈𝗇𝖺 𝖺 𝗅𝗈𝗌 𝗆𝗂𝖾𝗆𝖻𝗋𝗈𝗌
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗄𝗂𝖼𝗄 @usuario
+> ✨ 𝖤𝗑𝗉𝗎𝗅𝗌𝖺 𝖺 𝖺𝗅𝗀𝗎𝗂𝖾𝗇
+꒰🌕ᩚ꒱  ⣿   ☆   .𝖼𝗅𝗈𝗌𝖾
+> ✨ 𝖢𝗂𝖾𝗋𝗋𝖺 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗈𝗉𝖾𝗇
+> ✨ 𝖠𝖻𝗋𝖾 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗉𝗋𝗈𝗆𝗈𝗍𝖾 @usuario
+> ✨ 𝖣𝖺 𝖺𝖽𝗆𝗂𝗇𝗂𝗌𝗍𝗋𝖺𝖽𝗈𝗋
+꒰🌕ᩚ꒱  ⣿   ☆   .𝖽𝖾𝗆𝗈𝗍𝖾 @usuario
+> ✨ 𝖰𝗎𝗂𝗍𝖺 𝖺𝖽𝗆𝗂𝗇𝗂𝗌𝗍𝗋𝖺𝖽𝗈𝗋
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗆𝗎𝗍𝖾 @usuario
+> ✨ 𝖲𝗂𝗅𝖾𝗇𝖼𝗂𝖺 𝗎𝗌𝗎𝖺𝗋𝗂𝗈
+꒰🌕ᩚ꒱  ⣿   ☆   .𝗎𝗇𝗆𝗎𝗍𝖾 @usuario
+> ✨ 𝖰𝗎𝗂𝗍𝖺 𝗌𝗂𝗅𝖾𝗇𝖼𝗂𝗈`;
 
       await sock.sendMessage(jid, {
         image: { url: 'https://files.evogb.win/lOuoMM.jpg' },
